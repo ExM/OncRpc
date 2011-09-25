a6cbcc6e92b69628658425aa9986394585ed1719
ExM <ExplorerMachine@gmail.com>
2011-09-26 00:25:43 +0400

    added tests for begin switch read/write contexts

--- Xdr.Test/Example/CompleteFileTest.cs
+++ Xdr.Test/Example/CompleteFileTest.cs
@@ -122,6 +122,113 @@ namespace Xdr
 			s.Position = 0;
 			Assert.AreEqual(expected, s.ToArray());
 		}
+		
+		[Test]
+		public void Read_AttrMapping()
+		{
+			MemoryStream s = new MemoryStream();
+			s.Write(
+				0x00, 0x00, 0x00, 0x09,
+				0x73, 0x69, 0x6c, 0x6c,
+				0x79, 0x70, 0x72, 0x6f,
+				0x67, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x02,
+				0x00, 0x00, 0x00, 0x04,
+				0x6c, 0x69, 0x73, 0x70,
+				0x00, 0x00, 0x00, 0x04,
+				0x6a, 0x6f, 0x68, 0x6e,
+				0x00, 0x00, 0x00, 0x06,
+				0x28, 0x71, 0x75, 0x69,
+				0x74, 0x29, 0x00, 0x00);
+			s.Position = 0;
+			
+			ITranslator t = Translator.Create()
+				//.Map<FileType>(FileType.Read)
+				.Build();
+			
+			SyncStream ss = new SyncStream(s);
+			Reader r = t.CreateReader(ss);
+
+			CompleteFile result = null;
+
+			r.Read<CompleteFile>((val) => 
+			{
+				result = val;
+			}, (ex) => Assert.Fail("unexpected exception: {0}", ex));
+			
+			Assert.AreEqual(12*4, s.Position);
+			Assert.IsNotNull(result);
+
+//        0      00 00 00 09     ....     -- length of filename = 9
+//        4      73 69 6c 6c     sill     -- filename characters
+//        8      79 70 72 6f     ypro     -- ... and more characters ...
+//       12      67 00 00 00     g...     -- ... and 3 zero-bytes of fill
+			Assert.AreEqual("sillyprog", result.FileName);
+//       16      00 00 00 02     ....     -- filekind is EXEC = 2
+			Assert.AreEqual(FileKind.Exec, result.Type.Type);
+			Assert.IsNull(result.Type.Creator);
+//       20      00 00 00 04     ....     -- length of interpretor = 4
+//       24      6c 69 73 70     lisp     -- interpretor characters
+			Assert.AreEqual("lisp", result.Type.Interpretor);
+//       28      00 00 00 04     ....     -- length of owner = 4
+//       32      6a 6f 68 6e     john     -- owner characters
+			Assert.AreEqual("john", result.Owner);
+//       36      00 00 00 06     ....     -- length of file data = 6
+//       40      28 71 75 69     (qui     -- file data bytes ...
+//       44      74 29 00 00     t)..     -- ... and 2 zero-bytes of fill
+			Assert.IsNotNull(result.Data);
+			Assert.AreEqual(6, result.Data.Length);
+			Assert.AreEqual(0x28, result.Data[0]);
+			Assert.AreEqual(0x29, result.Data[5]);
+		}
+		
+		[Test]
+		public void Write_AttrMapping()
+		{
+			CompleteFile cf = new CompleteFile();
+			cf.FileName = "sillyprog";
+			cf.Type = new FileType();
+			cf.Type.Type = FileKind.Exec;
+			cf.Type.Interpretor = "lisp";
+			cf.Owner = "john";
+			cf.Data = new byte[] {0x28, 0x71, 0x75, 0x69, 0x74, 0x29};
+			
+			
+			MemoryStream s = new MemoryStream();
+			
+			
+			ITranslator t = Translator.Create()
+				//.Map<FileType>(FileType.Write)
+				.Build();
+			
+			SyncStream ss = new SyncStream(s);
+			Writer w = t.CreateWriter(ss);
+
+			w.Write<CompleteFile>(cf,
+				() => {},
+				(ex) => Assert.Fail("unexpected exception: {0}", ex));
+			
+			Assert.AreEqual(12*4, s.Position);
+			
+			byte[] expected = new byte[]
+			{
+				0x00, 0x00, 0x00, 0x09,
+				0x73, 0x69, 0x6c, 0x6c,
+				0x79, 0x70, 0x72, 0x6f,
+				0x67, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x02,
+				0x00, 0x00, 0x00, 0x04,
+				0x6c, 0x69, 0x73, 0x70,
+				0x00, 0x00, 0x00, 0x04,
+				0x6a, 0x6f, 0x68, 0x6e,
+				0x00, 0x00, 0x00, 0x06,
+				0x28, 0x71, 0x75, 0x69,
+				0x74, 0x29, 0x00, 0x00
+			};
+			
+			s.Position = 0;
+			Assert.AreEqual(expected, s.ToArray());
+		}
 
 	}
 }
